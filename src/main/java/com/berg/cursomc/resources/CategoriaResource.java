62c8ba8b6f173fe330fd2e33d4aaeb9beb4395bb
Eduardo Berg <duhberg@gmail.com>
2021-01-07 15:08:00 -0300

    REST testado

---
+++ src/main/java/com/berg/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.berg.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar()
+	{
+		return "Rest está funcionando!";
+	}
+		
+}
