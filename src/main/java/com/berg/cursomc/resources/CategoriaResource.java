2964f80346d1d08bcbbcc4ac582c0364636805fc
Eduardo Berg <duhberg@gmail.com>
2021-01-13 15:53:56 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/berg/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/berg/cursomc/resources/CategoriaResource.java
@@ -45,4 +45,11 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/berg/cursomc/services/CategoriaService.java
+++ src/main/java/com/berg/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.berg.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.berg.cursomc.domain.Categoria;
 import com.berg.cursomc.repositories.CategoriaRepository;
+import com.berg.cursomc.services.exceptions.DataIntegrityException;
 import com.berg.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,6 +34,19 @@ public class CategoriaService {
 	{
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id)
+	{
+		find(id);
+		try
+		{
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e)
+		{
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 
 }
