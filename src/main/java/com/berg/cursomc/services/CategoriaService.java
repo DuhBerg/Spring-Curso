24b109a5c5e4553fae9370132dbdd8315d9e56f5
Eduardo Berg <duhberg@gmail.com>
2021-01-14 18:46:31 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/berg/cursomc/services/CategoriaService.java
+++ src/main/java/com/berg/cursomc/services/CategoriaService.java
@@ -25,50 +25,47 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: "+id+ ", Tipo: " + Categoria.class.getName()));
-		
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
-	
-	public Categoria insert(Categoria obj)
-	{
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	public Categoria update(Categoria obj)
-	{
-		find(obj.getId());
-		return repo.save(obj);
+
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
-	public void delete(Integer id)
-	{
+
+	public void delete(Integer id) {
 		find(id);
-		try
-		{
+		try {
 			repo.deleteById(id);
-		}
-		catch(DataIntegrityViolationException e)
-		{
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 	}
-	
-	public List<Categoria> findAll()
-	{
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy, String direction)
-	{
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
-	public Categoria fromDTO(CategoriaDTO objDTO)
-	{
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
-		
+
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj)
+	{
+		newObj.setNome(obj.getNome());
 	}
 
 }
