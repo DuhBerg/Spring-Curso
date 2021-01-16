5d23d8b8a782b9d2324db6bb3aedd43fd5750304
Eduardo Berg <duhberg@gmail.com>
2021-01-16 00:16:37 -0300

    Revert "Busca de pedidos por nome e categorias"
    This reverts commit b15e5753bbee8de8f69928f8c48eee2efff8281a.

--- src/main/java/com/berg/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/berg/cursomc/repositories/ProdutoRepository.java
@@ -1,19 +1,12 @@
 package com.berg.cursomc.repositories;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.berg.cursomc.domain.Categoria;
 import com.berg.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
-	@Transactional(readOnly = true)
-	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
 }
