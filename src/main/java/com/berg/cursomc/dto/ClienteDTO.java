ac75f9b96f8f3d92849a95fc0056b445ad1cf9fc
Eduardo Berg <duhberg@gmail.com>
2021-01-16 00:18:46 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/berg/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/berg/cursomc/dto/ClienteDTO.java
@@ -8,9 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.berg.cursomc.domain.Cliente;
-import com.berg.cursomc.services.validation.ClienteUpdate;
 
-@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
