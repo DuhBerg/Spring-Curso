c2aa9a4f7a0e4da24d6d50db51517c8c27a456b6
Eduardo Berg <duhberg@gmail.com>
2021-01-16 00:54:00 -0300

    Subtotal e total do Pedido

--- src/main/java/com/berg/cursomc/domain/ItemPedido.java
+++ src/main/java/com/berg/cursomc/domain/ItemPedido.java
@@ -32,6 +32,12 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	public double getSubtotal()
+	{
+		return(preco - desconto) * quantidade;
+	}
+	
 
 	@JsonIgnore
 	public Pedido getPedido()
--- src/main/java/com/berg/cursomc/domain/Pedido.java
+++ src/main/java/com/berg/cursomc/domain/Pedido.java
@@ -54,6 +54,17 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	
+	public double getValorTotal()
+	{
+		double soma = 0;
+		for(ItemPedido ip : itens)
+		{
+			soma = soma + ip.getSubtotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
